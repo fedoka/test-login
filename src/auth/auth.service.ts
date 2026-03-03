d6d98e29e327bb120206c20bbefa73966ec69f14
fedoka <eder-ramtz@hotmail.com>
2026-03-03 15:27:05 -0600

    Refactor login logic to improve clarity and maintainability

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,41 +4,47 @@ import { LoginDto } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthService {
+  private readonly MAX_LOGIN_ATTEMPTS = 3;
+  private readonly LOCK_TIME = 5 * 60 * 1000; // 5 minutos
   constructor(private readonly usersService: UsersService) {}
   async login(loginDto: LoginDto) {
     const user = await this.usersService.findOneByEmail(loginDto.email);
     if (!user) {
       throw new BadRequestException('Usuario no encontrado');
     }
+    this.checkLock(user.lockedUntil);
     if (user.password !== loginDto.password) {
       await this.incrementAttempts(user.email, user.loginAttempts);
       throw new BadRequestException('Contraseña incorrecta');
     }
-    if (user.lockedUntil && new Date() < new Date(user.lockedUntil)) {
-      throw new BadRequestException(
-        'Cuenta bloqueada por intentos fallidos. Intente nuevamente en 5 minutos.',
-      );
-    }
     await this.resetAttempts(user.email);
     return { message: 'login exitoso' };
   }
-  async incrementAttempts(email: string, loginAttempts: number) {
-    if (loginAttempts >= 3) {
-      await this.lockAccount(email);
+  checkLock(lockedUntil: Date) {
+    if (lockedUntil && new Date() < new Date(lockedUntil)) {
       throw new BadRequestException(
-        'Cuenta bloqueada por intentos fallidos. Intente nuevamente en 5 minutos.',
+        `Cuenta bloqueada por intentos fallidos. Intente nuevamente en ${this.LOCK_TIME / 60000} minutos.`,
       );
     }
-    return this.usersService.updateUser(email, {
-      loginAttempts: loginAttempts + 1,
+  }
+  async incrementAttempts(email: string, loginAttempts: number) {
+    const newAttempts = loginAttempts + 1;
+    if (newAttempts >= this.MAX_LOGIN_ATTEMPTS) {
+      await this.lockAccount(email);
+      throw new BadRequestException(
+        `Cuenta bloqueada por intentos fallidos. Intente nuevamente en ${this.LOCK_TIME / 60000} minutos.`,
+      );
+    }
+    await this.usersService.updateUser(email, {
+      loginAttempts: newAttempts,
     });
   }
   async lockAccount(email: string) {
-    const lockedUntil = new Date(Date.now() + 5 * 60 * 1000);
-    return this.usersService.updateUser(email, { lockedUntil });
+    const lockedUntil = new Date(Date.now() + this.LOCK_TIME);
+    await this.usersService.updateUser(email, { lockedUntil });
   }
   async resetAttempts(email: string) {
-    return this.usersService.updateUser(email, {
+    await this.usersService.updateUser(email, {
       loginAttempts: 0,
       lockedUntil: null,
     });
